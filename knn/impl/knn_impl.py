eae50ee2044c2ff6f7a497e0f32030c936d61b66
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-11-01 23:12:31 +0300

    feat: functions for kernel

--- knn/impl/knn_impl.py
+++ knn/impl/knn_impl.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
+from knn.impl.service.kernel_function import uniform_function, gaussian_function, generalized_function, \
+    exponential_function
+
 
 class KNearestNeighbors:
     def __init__(self, base_n_neighbors=5, weights='uniform', kernel='uniform', metric='euclidean', min_neighbors=1,
@@ -25,93 +28,102 @@ class KNearestNeighbors:
         else:
             self.sample_weights = np.ones(len(x))
 
-        self.sample_weights = sample_weights if sample_weights is not None else np.ones(len(x))
         self.model.fit(x)
 
-    def predict(self, X):
-        """Предсказание меток классов для новых данных."""
+    def predict(self, x):
         predictions = []
-        for point in X:
+        for point in x:
             distances, indices = self.model.kneighbors([point], n_neighbors=self.base_n_neighbors)
             n_neighbors = self._get_window_size(distances[0])
             distances, indices = self.model.kneighbors([point], n_neighbors=n_neighbors)
-            if self.weights == 'uniform':
-                predictions.append(self._predict_uniform(indices))
-            elif self.weights == 'distance':
-                predictions.append(self._predict_distance(distances, indices))
+            predictions = self._predictor(predictions, indices, distances)
+
         return np.array(predictions)
 
+    def _predictor(self, predictions, indices, distances):
+        if self.weights == 'uniform':
+            predictions.append(self._receive_most_probable_uniform(indices))
+        elif self.weights == 'distance':
+            predictions.append(self._receive_most_probable_distance(distances, indices))
+        return predictions
+
     def _get_window_size(self, distances):
-        """Определение размера окна в зависимости от выбранной стратегии."""
         if self.dynamic_window:
             return max(self.min_neighbors, len(distances))
         else:
             return self.base_n_neighbors
 
-    def _predict_uniform(self, indices):
+    def _receive_most_probable_uniform(self, indices):
         weighted_votes = {}
         neighbors = indices[0]
-        for neighbor in neighbors:
-            if isinstance(self.y_train, pd.Series):
-                label = self.y_train.iloc[neighbor]
-            else:
-                label = self.y_train[neighbor]
-
-            if self.sample_weights is not None:
-                if isinstance(self.sample_weights, pd.Series):
-                    weight = self.sample_weights.iloc[neighbor]
-                else:
-                    weight = self.sample_weights[neighbor]
-            else:
-                weight = 1
-
-            weighted_votes[label] = weighted_votes.get(label, 0) + weight
+        weighted_votes = self._check_all_neighbors_for_form(neighbors, weighted_votes)
 
         return max(weighted_votes, key=weighted_votes.get)
 
-    def _predict_distance(self, distances, indices):
-        """Предсказание с использованием взвешенного голосования по расстоянию с учётом априорных весов."""
+    def _check_all_neighbors_for_form(self, neighbors, weighted_votes):
+        for neighbor in neighbors:
+            label = self._update_label(neighbor)
+            weight = self._receive_weights(neighbor)
+
+            weighted_votes[label] = weighted_votes.get(label, 0) + weight
+
+        return weighted_votes
+
+    def _update_label(self, neighbor):
+        if isinstance(self.y_train, pd.Series):
+            return self.y_train.iloc[neighbor]
+        else:
+            return self.y_train[neighbor]
+
+    def _receive_weights(self, neighbor):
+        if self.sample_weights is not None:
+            return self._update_weights(neighbor)
+        else:
+            return 1
+
+    def _update_weights(self, neighbor):
+        if isinstance(self.sample_weights, pd.Series):
+            return self.sample_weights.iloc[neighbor]
+        else:
+            return self.sample_weights[neighbor]
+
+    def _receive_most_probable_distance(self, distances, indices):
         predictions = []
-        for i, neighbors in enumerate(indices):
-            weighted_votes = {}
-            for j, neighbor in enumerate(neighbors):
-                distance = distances[i][j]
-                kernel_weight = self._apply_kernel(distance)
-                label = self.y_train.iloc[neighbor]
-                weight = kernel_weight * self.sample_weights[neighbor]
-                if label in weighted_votes:
-                    weighted_votes[label] += weight
-                else:
-                    weighted_votes[label] = weight
-            predictions.append(max(weighted_votes, key=weighted_votes.get))
+        predictions = self._check_all_indexes(distances, indices, predictions)
         return predictions
 
-    def _apply_kernel(self, distance):
-        """Применение выбранного ядра для вычисления веса."""
+    def _check_all_indexes(self, distances, indices, predictions):
+        for i, neighbors in enumerate(indices):
+            weighted_votes = {}
+            weighted_votes = self._check_all_neighbors_for_distance(distances, neighbors, weighted_votes, i)
+            predictions.append(max(weighted_votes, key=weighted_votes.get))
+
+        return predictions
+
+    def _check_all_neighbors_for_distance(self, distances, neighbors, weighted_votes, ind):
+        for j, neighbor in enumerate(neighbors):
+            distance = distances[ind][j]
+            kernel_weight = self._add_kernel(distance)
+            label = self.y_train.iloc[neighbor]
+            weight = kernel_weight * self.sample_weights[neighbor]
+            if label in weighted_votes:
+                weighted_votes[label] += weight
+            else:
+                weighted_votes[label] = weight
+
+        return weighted_votes
+
+    def _add_kernel(self, distance):
         if self.kernel == 'uniform':
-            return 1
+            return uniform_function()
         elif self.kernel == 'gaussian':
-            return np.exp(-distance ** 2)
+            return gaussian_function(distance)
         elif self.kernel == 'generalized':
-            return (1 - distance) ** 2
+            return generalized_function(distance)
         elif self.kernel == 'exponential':
-            return np.exp(-distance)
+            return exponential_function(distance)
         else:
-            raise ValueError(f"Unknown kernel: {self.kernel}")
-
-    def set_metric(self, metric, p=None):
-        """Установка метрики для вычисления расстояний."""
-        if metric == 'minkowski' and p is not None:
-            self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric=metric, p=p)
-        elif metric == 'chebyshev':
-            self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric='chebyshev')
-        else:
-            self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric=metric)
-
-    def set_window(self, n_neighbors):
-        """Установка базового размера окна (количества соседей)."""
-        self.base_n_neighbors = n_neighbors
-        self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric=self.metric)
+            raise ValueError(f"Incorrect kernel: {self.kernel}")
 
     def get_params(self, deep=True):
         return {
@@ -120,6 +132,18 @@ class KNearestNeighbors:
             'metric': self.metric,
             'dynamic_window': self.dynamic_window
         }
+
+    def set_metric(self, metric, p=None):
+        if metric == 'minkowski' and p is not None:
+            self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric=metric, p=p)
+        elif metric == 'chebyshev':
+            self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric='chebyshev')
+        else:
+            self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric=metric)
+
+    def set_window(self, n_neighbors):
+        self.base_n_neighbors = n_neighbors
+        self.model = NearestNeighbors(n_neighbors=self.base_n_neighbors, metric=self.metric)
 
     def set_params(self, **params):
         for param, value in params.items():
